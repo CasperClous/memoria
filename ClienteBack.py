5f5703f638c28d9d4d9014bcd1876899fd0e6e07
CasperClous <45837640+CasperClous@users.noreply.github.com>
2021-11-17 23:08:12 -0300

    Update ClienteBack.py

--- ClienteBack.py
+++ ClienteBack.py
@@ -1,44 +1,35 @@
-from _thread import *
 import mysql.connector
 from mysql.connector import errorcode
 import socketserver
-global cnx
-global curs
 
 
-def insert1(FROM, primerReceived, penultimoReceived, MSGID, AUTHRESULT):
-    ConectarBaseDeDatos()
+
+def insert1(FROM, primerReceived, penultimoReceived, MSGID, AUTHRESULT, cnx, curs):
     try:
         add_Correo = "INSERT INTO Coleccion (FROMM, PrimerReceived, PenultimoReceived, MSGID, AuthenticationResult) VALUES (%s,%s,%s,%s,%s)"
         val = (FROM, primerReceived, penultimoReceived, MSGID, AUTHRESULT)
         curs.execute(add_Correo, val)
         cnx.commit()
-        cnx.close()
     except mysql.connector.Error as error:
         print(error)
-        cnx.close()
 
 
-def insert2(FROM, primerReceived, MSGID, AUTHRESULT):
-    ConectarBaseDeDatos()
+def insert2(FROM, primerReceived, MSGID, AUTHRESULT, cnx, curs):
     try:
         add_Correo = "INSERT INTO Coleccion (FROMM, PrimerReceived, MSGID, AuthenticationResult) VALUES (%s,%s,%s,%s)"
         val = (FROM, primerReceived, MSGID, AUTHRESULT)
         curs.execute(add_Correo, val)
         cnx.commit()
-        cnx.close()
     except mysql.connector.Error as error:
         print(error)
-        cnx.close()
 
 
 def ConectarBaseDeDatos():
-    global cnx
-    global curs
+    cnx = ''
     try:
-        cnx = mysql.connector.connect(user='addCorreo', password='CorreoV@lidator2021', host='192.168.100.86',
-                                      database='Validator')
+        cnx = mysql.connector.connect(user='addCorreo', password='CorreoV@lidator2021', host='192.168.100.86', database='Validator')
         curs = cnx.cursor()
+        return cnx, curs
     except mysql.connector.Error as err:
         if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
             print("Something is wrong with your user name or password")
@@ -52,6 +43,7 @@ def ConectarBaseDeDatos():
 class MyTCPHandler(socketserver.BaseRequestHandler):
     def handle(self):
         separador = " ##### "
+        cnx, curs = ConectarBaseDeDatos()
         while True:
             datos = self.request.recv(1024).decode()
             datos = datos.split(separador)
@@ -64,21 +56,22 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
                     penultimoReceived = datos[3].strip(separador)
                     MSGID = datos[4].strip(separador)
                     AUTHRESULT = datos[5].strip(separador)
-                    insert1(FROMM, primerReceived, penultimoReceived, MSGID, AUTHRESULT)
+                    insert1(FROMM, primerReceived, penultimoReceived, MSGID, AUTHRESULT, cnx, curs)
                 elif int(datos[0].strip(separador)) == 2:
                     FROMM = datos[1].strip(separador)
                     primerReceived = datos[2].strip(separador)
                     MSGID = datos[3].strip(separador)
                     AUTHRESULT = datos[4].strip(separador)
-                    insert2(FROMM, primerReceived, MSGID, AUTHRESULT)
+                    insert2(FROMM, primerReceived, MSGID, AUTHRESULT, cnx, curs)
                 else:
+                    cnx.close()
                     connection.close()
 
 if __name__ == "__main__":
     HOST, PORT = '192.168.100.86', 10001
 
     # Create the server, binding to localhost on port 9999
-    server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
+    server = socketserver.ThreadingTCPServer((HOST, PORT), MyTCPHandler)
 
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
